a300c9fc87c875857003e8aa8d6b219d49f9a9d4
Tozzi <nicholastozzi@gmail.com>
2022-12-12 23:51:42 -0500

    added files

---
+++ Develop/script.js
@@ -0,0 +1,50 @@
+// Declare Variables
+var timeDisplayEl = $("#currentDay");
+var saveBtn = $(".saveBtn");
+//I want to get the id from the divs that contain the row-block class
+var currentTime = dayjs.hour;
+
+function displayDate() {
+  //code to display the current date in the header of the page.
+  var currentDate = dayjs().format("MMM DD, YYYY");
+  timeDisplayEl.text(currentDate);
+}
+displayDate();
+
+//Wrap all code that interacts with the DOM in a call the code isn't run until the browser has finished rendering all the elementsto jQuery to ensure that
+$(document).ready(function () {
+  //Get the items from local storage
+  for (let i = 9; i < 18; i++) {
+    var scheduledTimeSlot = localStorage.getItem(i);
+    $(`#${i}`).children("textarea").val(scheduledTimeSlot);
+    changeColor(i); //call during the for loop so that it sets color/Storage at same time
+  }
+
+  saveBtn.each(function () {
+    //refers to EACH save button
+    $(this).on("click", function () {
+      //jquery's version of event listener.
+
+      var textAreaId = $(this).parent().attr("id"); // key
+      var localInput = $(this).siblings("textarea").val(); // value
+      localStorage.setItem(textAreaId, localInput);
+      console.log(localStorage.getItem(textAreaId));
+    });
+  });
+
+  function changeColor(i) {
+    if (currentTime < i) {
+      $(`#${i}`).removeClass("future");
+      $(`#${i}`).removeClass("present");
+      $(`#${i}`).addClass("past");
+    } else if (currentTime === i) {
+      $(`#${i}`).removeClass("past");
+      $(`#${i}`).addClass("present");
+      $(`#${i}`).removeClass("future");
+    } else {
+      $(`#${i}`).addClass("past");
+      $(`#${i}`).removeClass("present");
+      $(`#${i}`).removeClass("future");
+    }
+  }
+});
